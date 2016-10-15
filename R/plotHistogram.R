00c2fbfd32f619d8aa13b5d0985824ff4adda5d2
Diego Diez <diego10ruiz@gmail.com>
2016-10-15 23:52:00 +0900

    Simplify plotHistogram(): removed groupCol argument. Added EList and DGEList methods to plotHistogram(). Removed legacy code.

--- R/plotHistogram.R
+++ R/plotHistogram.R
@@ -4,7 +4,6 @@
 #'
 #' @param x     object from which to extract a matrix to plot the histogram.
 #' @param group grouping variable (columns).
-#' @param ... further arguments passed to methods.
 #'
 #' @return
 #' @export
@@ -12,7 +11,7 @@
 #' @docType methods
 #' @rdname plotHistogram-methods
 #' @examples
-setGeneric("plotHistogram", function(x, group = NULL, ...)
+setGeneric("plotHistogram", function(x, group = NULL)
   standardGeneric("plotHistogram")
 )
 
@@ -21,6 +20,7 @@ setGeneric("plotHistogram", function(x, group = NULL, ...)
 setMethod("plotHistogram", "matrix",
 function(x, group = NULL) {
   d <- melt(x, varnames = c("gene", "sample"))
+
   if (!is.null(group)) {
     names(group) <- colnames(group)
     d$group <- group[d$sample]
@@ -33,45 +33,23 @@ function(x, group = NULL) {
     g
 })
 
-#' @param groupCol Column name from which extract grouping variable.
-#'
 #' @rdname plotHistogram-methods
 #' @aliases plotHistogram,ExpressionSet-method
 setMethod("plotHistogram", "ExpressionSet",
-function(x, group = NULL, groupCol = NULL) {
-  y <- exprs(x)
-
-  if (!is.null(group)) {
-    s <- sampleNames(x)
-    names(group) <- sampleNames(x)
-  } else {
-    if (!is.null(groupCol)) {
-      group <- x[[groupCol]]
-    }
-  }
+function(x, group = NULL) {
+  plotHistogram(exprs(x), group = group)
 })
 
-# plotHistogram <- function(x) {
-#   if (class(x) == "DGEList") {
-#     y <- x$counts
-#     group <- x$samples$group
-#     names(group) <- rownames(x$samples)
-#   }
-#
-#   if (class(x) == "EList") {
-#     y <- x$E
-#     group <- x$targets$group
-#     names(group) <- rownames(x$targets)
-#   }
-#
-#   if (class(x) == "ExpressionSet") {
-#     y <- exprs(x)
-#     s <- sampleNames(x)
-#     group <- x$group
-#     names(group) <- sampleNames(x)
-#   }
-#
-#   d <- melt(y, varnames = c("protein", "sample"))
-#   d$group <- group[d$sample]
-#   ggplot(d, aes_string(x = "value")) + geom_histogram(bins = 25) + facet_wrap(~group) + theme(aspect.ratio = 1)
-# }
+#' @rdname plotHistogram-methods
+#' @aliases plotHistogram,EList-method
+setMethod("plotHistogram", "EList",
+  function(x, group = NULL) {
+    plotHistogram(x$E, group = group)
+})
+
+#' @rdname plotHistogram-methods
+#' @aliases plotHistogram,DGEList-method
+setMethod("plotHistogram", "DGEList",
+function(x, group = NULL) {
+  plotHistogram(x$counts, group = group)
+})
