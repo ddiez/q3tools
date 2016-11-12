a6db1d5c93b3dee4829643750cbc2b2d1e01cd09
Diego Diez <diego10ruiz@gmail.com>
2016-11-12 18:12:04 +0900

    Fixed typo.

--- tests/testthat/test_impute.R
+++ tests/testthat/test_impute.R
@@ -1,6 +1,6 @@
 library(q3tools)
 
-context("Imput return value")
+context("Impute return value")
 test_that("imputeGroup returns matrix", {
   expect_is(imputeGroup(matrix(NA, ncol = 2, nrow = 2), c(1, 2)), "matrix")
   expect_false(any(is.na(imputeGroup(matrix(NA, ncol = 2, nrow = 2), c(1, 2)))))
